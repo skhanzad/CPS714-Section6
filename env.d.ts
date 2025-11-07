adef7933c2263a0450a6194263344ccc6c8ad422
Jackie <li.jackie565@gmail.com>
2025-11-07 13:02:38 -0500

    added schema

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    POSTGRES_HOST: string;
+    POSTGRES_PORT: number;
+    POSTGRES_USER: string;
+    POSTGRES_PASSWORD: string;
+    POSTGRES_DATABASE: string;
+  }
+}

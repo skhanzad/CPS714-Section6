bc068ada4d64564681e3830e0f23741b7dc04095
NumairJ <98196808+NumairJ@users.noreply.github.com>
2025-11-10 21:30:49 +0000

    Updated Readme, created logout button component and API, created comments

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -4,12 +4,14 @@ import User from "@/auth/User";
 export async function POST(req: Request) {
     try {
         const { firstName, lastName, email, studentId, password } = await req.json();
-        
+
+        //Determines if user already exists with studentId. Each account must have unique studentId
         const existingUser = await User.userexist(studentId);
         if (existingUser) {
             return NextResponse.json({ error: "User with ID already exists" }, { status: 409 });
         }
 
+        //Creates new user
         const newUser = await User.signup(firstName, lastName, email, studentId, password);
         if (newUser === null) {
             return NextResponse.json({ error: "Sign Up failed" }, { status: 400 });

b6292378d97f65703de229147bd84718474946c1
sayeed <asayeed216@gmail.com>
2025-11-13 13:23:27 -0500

    fix w sendgrid

--- src/utils/sendgrid.ts
+++ src/utils/sendgrid.ts
@@ -5,13 +5,15 @@ const FROM_EMAIL = process.env.SENDGRID_FROM_EMAIL;
 
 if (API_KEY) sgMail.setApiKey(API_KEY);
 
-export async function sendEmail(to: string, subject: string, html: string) {
+export async function sendEmail(to: string, subject: string, html: string)
+{
     if (!API_KEY || !FROM_EMAIL) throw new Error("SendGrid not configured");
     const msg = { to, from: FROM_EMAIL, subject, html };
     return sgMail.send(msg);
 }
 
-export async function sendBroadcast(toList: string[], subject: string, html: string) {
+export async function sendBroadcast(toList: string[], subject: string, html: string)
+{
     if (!API_KEY || !FROM_EMAIL) throw new Error("SendGrid not configured");
     const msgs = toList.map(to => ({ to, from: FROM_EMAIL, subject, html }));
     return sgMail.send(msgs);

4dbe2e2fec115105d930cd24e76abfaa8efa3ab3
sayeed <asayeed216@gmail.com>
2025-11-18 13:56:25 -0500

    sendgrid configured

--- src/utils/sendgrid.ts
+++ src/utils/sendgrid.ts
@@ -3,18 +3,20 @@ import sgMail from "@sendgrid/mail";
 const API_KEY = process.env.SENDGRID_API_KEY;
 const FROM_EMAIL = process.env.SENDGRID_FROM_EMAIL;
 
-if (API_KEY) sgMail.setApiKey(API_KEY);
-
-export async function sendEmail(to: string, subject: string, html: string)
-{
-    if (!API_KEY || !FROM_EMAIL) throw new Error("SendGrid not configured");
-    const msg = { to, from: FROM_EMAIL, subject, html };
-    return sgMail.send(msg);
+if (API_KEY) {
+  sgMail.setApiKey(API_KEY);
+} else {
+  console.warn("SENDGRID_API_KEY not set; emails will fail.");
 }
 
-export async function sendBroadcast(toList: string[], subject: string, html: string)
-{
-    if (!API_KEY || !FROM_EMAIL) throw new Error("SendGrid not configured");
-    const msgs = toList.map(to => ({ to, from: FROM_EMAIL, subject, html }));
-    return sgMail.send(msgs);
+export async function sendEmail(to: string, subject: string, html: string) {
+  if (!API_KEY || !FROM_EMAIL) throw new Error("SendGrid not configured");
+  const msg = { to, from: FROM_EMAIL, subject, html };
+  return sgMail.send(msg);
+}
+
+export async function sendBroadcast(toList: string[], subject: string, html: string) {
+  if (!API_KEY || !FROM_EMAIL) throw new Error("SendGrid not configured");
+  const msgs = toList.map(to => ({ to, from: FROM_EMAIL, subject, html }));
+  return sgMail.send(msgs);
 }
